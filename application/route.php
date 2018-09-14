3a6251e2d871a813d3dd9cbdc2ae9b9e6ce64059
yanghao_000 <yanghao_000@126.com>
2018-09-14 17:56:02 +0800

    d

--- application/route.php
+++ application/route.php
@@ -22,4 +22,15 @@ return [
     'blog/:year/:month' => ['test/blog/archive', ['method' => 'get'], ['year' => '\d{4}', 'month' => '\d{2}']],
     'blog/:id'          => ['test/blog/get', ['method' => 'get'], ['id' => '\d+']],
     'blog/:name'        => ['test/blog/read', ['method' => 'get'], ['name' => '\w+']],
+
+    '__pattern__'         => [
+        'id'    => '\d+',
+    ],
+    'user/index'      => 'index/user/index',
+    'user/create'     => 'index/user/create',
+    'user/add'        => 'index/user/add',
+    'user/add_list'   => 'index/user/addList',    
+    'user/update/:id' => 'index/user/update',
+    'user/delete/:id' => 'index/user/delete',
+    'user/:id'        => 'index/user/read',
 ];
